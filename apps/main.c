4de31e7aea900be99492fca352d53a2fce6f6d83
Yevhen Mazur <yevhen.mazur@plvision.eu>
2025-12-10 00:16:29 +0100

    add general structure for main.c and read user data options

--- apps/main.c
+++ apps/main.c
@@ -5,7 +5,6 @@
 
 #include "calc.h"
 
-
 void print_menu() {
     printf("\n===== CALCULATOR =====\n");
     printf("1. Addition (a + b)\n");
@@ -18,7 +17,100 @@ void print_menu() {
     printf("Choose option: ");
 }
 
+double read_double(const char *prompt) {
+    double value;
+    int result;
+
+    while (true) {
+        printf("%s", prompt);
+        result = scanf("%lf", &value);
+
+        if (result == 1) {
+            return value;
+        }
+
+        printf("Error: please enter a valid number.\n");
+    }
+}
+
+int read_int(const char *prompt) {
+    int value;
+    int result;
+
+    while (true) {
+        printf("%s", prompt);
+        result = scanf("%d", &value);
+
+        if (result == 1) {
+            return value;
+        }
+
+        printf("Error: please enter a valid integer.\n");
+    }
+}
 
 int main() {
+    while(true) {
+        print_menu();
+        int option = read_int("");
+
+        if (option == 0) {
+            printf("Programe has finished!\n");
+            break;
+        }
+
+        switch(option) {
+            case 1: {
+                double a = read_double("Enter a: ");
+                double b = read_double("Enter b: ");
+                break;
+            }
+            case 2: {
+                double a = read_double("Enter a: ");
+                double b = read_double("Enter b: ");
+                break;
+            }
+            case 3: {
+                double a = read_double("Enter a: ");
+                double b = read_double("Enter b: ");
+                break;
+            }
+            case 4: {
+                double a = read_double("Enter a: ");
+                double b = read_double("Enter b: ");
+                if (b == 0.) {
+                    pritnf("B cant be equal 0\n");
+                }
+                else {
+                
+                }
+                break;
+            }
+            case 5: {
+                int n = read_int("Enter int n >= 0: ");
+                if (n < 0) {
+                    preintf("N should be greater than 0\n");
+                }
+                else {
+
+                }
+                break;
+            }
+            case 6: {
+                double x = read_double("Enter double x >= 0: ");
+                if (x < 0.) {
+                    preintf("N should be greater than 0\n");
+                }
+                else {
+
+                }
+                break;
+            }
+            default: {
+                printf("Invalid option\n");
+                break;
+            }
+        }
+    }
     return 0;
 }
