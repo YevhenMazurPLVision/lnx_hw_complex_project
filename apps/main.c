bf27e52d7725aa1df3449fabc090d54ffa5c205c
Yevhen Mazur <yevhen.mazur@plvision.eu>
2025-12-09 21:05:26 +0100

    menu for main.c

---
+++ apps/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "calc.h"
+
+
+void print_menu() {
+    printf("\n===== CALCULATOR =====\n");
+    printf("1. Addition (a + b)\n");
+    printf("2. Subtraction (a - b)\n");
+    printf("3. Multiplication (a * b)\n");
+    printf("4. Division (a / b)\n");
+    printf("5. Factorial (n!)\n");
+    printf("6. Square root (sqrt(x))\n");
+    printf("0. Exit\n");
+    printf("Choose option: ");
+}
+
+
+int main() {
+    return 0;
+}
