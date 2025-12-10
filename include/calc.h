f147217e794276cd21425043bdf9d31fff4ffd9d
Yevhen Mazur <yevhen.mazur@plvision.eu>
2025-12-10 11:17:54 +0100

    Finish  task 1. add clean function

---
+++ include/calc.h
@@ -0,0 +1,11 @@
+#ifndef CALC_H
+#define CALC_H
+
+void calc_add(double a, double b);
+void calc_sub(double a, double b);
+void calc_mul(double a, double b);
+void calc_div(double a, double b);
+void calc_factorial(int n);
+void calc_sqrt(double x);
+
+#endif
